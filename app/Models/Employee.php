7a3d7dc4c509af768c53d1e3c90bed293300edd9
pms007 <pymyoswe007@gmail.com>
2024-02-20 10:12:44 +0700

    multi tentancy

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -30,4 +30,9 @@ class Employee extends Model
     {
         return $this->belongsTo(Department::class);
     }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
